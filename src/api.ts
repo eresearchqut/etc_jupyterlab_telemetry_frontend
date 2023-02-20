ef8e816c5bd0d72c4ec35aee7ad826de22be9854
Mitchel Haring <mitchel.haring@gmail.com>
2023-02-20 10:21:12 +1000

    auto format

--- src/api.ts
+++ src/api.ts
@@ -1,49 +1,44 @@
 export class AWSAPIGatewayWrapper {
-
     private _url: string;
     private _bucket: string;
     private _path: string;
     private _retry: number | null;
     private _errorHandler: (e: any) => void | null
-
-    constructor(
-        { url, bucket, path, retry = 1000, errorHandler = console.error }:
-            {
-                url: string,
-                bucket: string,
-                path: string,
-                retry?: number | null,
-                errorHandler?: (e: any) => void | null
-            }) {
-
+    constructor({
+        url,
+        bucket,
+        path,
+        retry = 1000,
+        errorHandler = console.error
+    }: {
+        url: string,
+        bucket: string,
+        path: string,
+        retry ? : number | null,
+        errorHandler ? : (e: any) => void | null
+    }) {
         this.request = this.request.bind(this);
         this.requestAsync = this.requestAsync.bind(this);
-
         this._url = url;
         this._bucket = bucket;
         this._path = path;
         this._retry = retry;
         this._errorHandler = errorHandler;
     }
-
     request(data: any): void {
         (async () => {
             try {
                 await this.requestAsync(data);
-            }
-            catch (e) {
-
+            } catch (e) {
                 if (typeof this._retry == "number") {
                     setTimeout(this.request, this._retry, data);
                 }
             }
         })();
     }
-
-    async requestAsync(data: any): Promise<Response> {
+    async requestAsync(data: any): Promise < Response > {
         let response: Response;
         try {
-
             response = await fetch([this._url, this._bucket, this._path].join("/"), {
                 method: "POST",
                 mode: "cors",
@@ -55,7 +50,6 @@ export class AWSAPIGatewayWrapper {
                 referrerPolicy: "no-referrer",
                 body: JSON.stringify(data)
             });
-
             if (response.status !== 200) {
                 throw new Error(JSON.stringify({
                     "response.status": response.status,
@@ -63,16 +57,12 @@ export class AWSAPIGatewayWrapper {
                     "response.text": await response.text()
                 }));
             }
-
             return response;
-        }
-        catch (e) {
+        } catch (e) {
             if (typeof this._errorHandler == "function") {
                 this._errorHandler(e);
             }
-
             throw e;
         }
     }
 }
-
--- src/index.ts
+++ src/index.ts
@@ -1,167 +1,116 @@
 import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin
+    JupyterFrontEnd,
+    JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
 import {
-  ServerConnection
+    ServerConnection
 } from '@jupyterlab/services';
-
 import {
-  INotebookTracker,
-  NotebookPanel
+    INotebookTracker,
+    NotebookPanel
 } from '@jupyterlab/notebook';
-
 import {
-  IETCJupyterLabTelemetryLibraryFactory
+    IETCJupyterLabTelemetryLibraryFactory
 } from '@educational-technology-collective/etc_jupyterlab_telemetry_library';
-
 import {
-  IETCJupyterLabNotebookStateProvider
+    IETCJupyterLabNotebookStateProvider
 } from "@educational-technology-collective/etc_jupyterlab_notebook_state_provider";
-
-
-import { AWSAPIGatewayWrapper } from './api';
-
+import {
+    AWSAPIGatewayWrapper
+} from './api';
 
 function getTelemetryHandler(baseUrl: string, stateProvider: IETCJupyterLabNotebookStateProvider) {
-  let awsAPIGatewayWrapper: AWSAPIGatewayWrapper;
-
-  if (baseUrl.includes('jupyter.dev.qutanalytics.io')) {
-    awsAPIGatewayWrapper = new AWSAPIGatewayWrapper({
-      url: 'https://telemetry.dev.qutanalytics.io',
-      bucket: 'jupyterhub-telementry-dev-collector',
-      path: '2022s2'
-    });
-  } else if (baseUrl.includes('jupyter.qutanalytics.io')) {
-    awsAPIGatewayWrapper = new AWSAPIGatewayWrapper({
-      url: 'https://telemetry.qutanalytics.io',
-      bucket: 'jupyterhub-telementry-prod-collector',
-      path: '2022s2'
-    });
-  }
-
-  let remoteObserve = async (sender: any, args: any) => {
-    try {
-      let notebookPanel = args.notebookPanel;
-      let notebookState = stateProvider.getNotebookState({notebookPanel});
-      delete args.notebookPanel;
-
-      // override user's environment variables
-      if (args.environ) {
-        let environ = {
-          "hostname": args.environ.HOSTNAME,
-          "user": args.environ.JUPYTERHUB_USER,
-          "pwd": args.environ.PWD
-        };
-        args.environ = environ;
-      }
-      if (args.eventName == 'open_notebook') {
-        delete args.meta;
-      }
-
-      let data = {
-        "event": args,
-        "state": notebookState
-      };
-      if (awsAPIGatewayWrapper) {
-        await awsAPIGatewayWrapper.requestAsync(data);
-      } else {
-        console.log(data);
-      }
+    let awsAPIGatewayWrapper: AWSAPIGatewayWrapper;
+    if (baseUrl.includes('jupyter.dev.qutanalytics.io')) {
+        awsAPIGatewayWrapper = new AWSAPIGatewayWrapper({
+            url: 'https://telemetry.dev.qutanalytics.io',
+            bucket: 'jupyterhub-telementry-dev-collector',
+            path: '2022s2'
+        });
+    } else if (baseUrl.includes('jupyter.qutanalytics.io')) {
+        awsAPIGatewayWrapper = new AWSAPIGatewayWrapper({
+            url: 'https://telemetry.qutanalytics.io',
+            bucket: 'jupyterhub-telementry-prod-collector',
+            path: '2022s2'
+        });
     }
-    catch (e) {
-      console.error(e);
-    }
-  };
-
-  return remoteObserve;
+    let remoteObserve = async (sender: any, args: any) => {
+        try {
+            let notebookPanel = args.notebookPanel;
+            let notebookState = stateProvider.getNotebookState({
+                notebookPanel
+            });
+            delete args.notebookPanel;
+            // override user's environment variables
+            if (args.environ) {
+                let environ = {
+                    "hostname": args.environ.HOSTNAME,
+                    "user": args.environ.JUPYTERHUB_USER,
+                    "pwd": args.environ.PWD
+                };
+                args.environ = environ;
+            }
+            if (args.eventName == 'open_notebook') {
+                delete args.meta;
+            }
+            let data = {
+                "event": args,
+                "state": notebookState
+            };
+            if (awsAPIGatewayWrapper) {
+                await awsAPIGatewayWrapper.requestAsync(data);
+            } else {
+                console.log(data);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    };
+    return remoteObserve;
 }
-
-const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'etc-frontend:plugin',
-  autoStart: true,
-  requires: [
-    INotebookTracker,
-    IETCJupyterLabTelemetryLibraryFactory,
-    IETCJupyterLabNotebookStateProvider
-  ],
-  activate: (
-    app: JupyterFrontEnd,
-    notebookTracker: INotebookTracker,
-    etcJupyterLabTelemetryLibraryFactory: IETCJupyterLabTelemetryLibraryFactory,
-    etcJupyterLabNotebookStateProvider: IETCJupyterLabNotebookStateProvider
-  ) => {
-    (async () => {
-      await app.started;
-
-      const settings = ServerConnection.makeSettings();
-      const baseUrl = settings.baseUrl;
-      let telemetryHandler = getTelemetryHandler(baseUrl, etcJupyterLabNotebookStateProvider);
-
-      try {
-        notebookTracker.widgetAdded.connect(
-          (async (sender: INotebookTracker, notebookPanel: NotebookPanel) => {
-            //  Handlers must be attached immediately in order to detect early events, hence we do not want to await the appearance of the Notebook.
-
-            let etcJupyterLabTelemetryLibrary =
-              etcJupyterLabTelemetryLibraryFactory.create({ notebookPanel });
-
-            etcJupyterLabNotebookStateProvider.addNotebookPanel({ notebookPanel });
-
-            etcJupyterLabTelemetryLibrary.notebookClipboardEvent.notebookClipboardCopied.connect(
-	      telemetryHandler 
-            );
-            etcJupyterLabTelemetryLibrary.notebookClipboardEvent.notebookClipboardCut.connect(
-	      telemetryHandler 
-            );
-            etcJupyterLabTelemetryLibrary.notebookClipboardEvent.notebookClipboardPasted.connect(
-	      telemetryHandler 
-            );
-
-            etcJupyterLabTelemetryLibrary.notebookVisibilityEvent.notebookVisible.connect(
-	      telemetryHandler 
-            );
-            etcJupyterLabTelemetryLibrary.notebookVisibilityEvent.notebookHidden.connect(
-	      telemetryHandler 
-            );
-
-            etcJupyterLabTelemetryLibrary.notebookOpenEvent.notebookOpened.connect(
-	      telemetryHandler 
-            );
-            etcJupyterLabTelemetryLibrary.notebookCloseEvent.notebookClosed.connect(
-	      telemetryHandler 
-            );
-            etcJupyterLabTelemetryLibrary.notebookSaveEvent.notebookSaved.connect(
-	      telemetryHandler 
-            );
-            etcJupyterLabTelemetryLibrary.notebookScrollEvent.notebookScrolled.connect(
-	      telemetryHandler 
-            );
-
-            etcJupyterLabTelemetryLibrary.activeCellChangeEvent.activeCellChanged.connect(
-	      telemetryHandler 
-            );
-            etcJupyterLabTelemetryLibrary.cellAddEvent.cellAdded.connect(
-	      telemetryHandler 
-            );
-            etcJupyterLabTelemetryLibrary.cellRemoveEvent.cellRemoved.connect(
-	      telemetryHandler 
-            );
-            etcJupyterLabTelemetryLibrary.cellExecutionEvent.cellExecuted.connect(
-	      telemetryHandler 
-            );
-            etcJupyterLabTelemetryLibrary.cellErrorEvent.cellErrored.connect(
-	      telemetryHandler 
-            );
-          })
-        );
-      } catch (e) {
-        console.error(e);
-      }
-    })();
-  }
+const plugin: JupyterFrontEndPlugin < void > = {
+    id: 'etc-frontend:plugin',
+    autoStart: true,
+    requires: [
+        INotebookTracker,
+        IETCJupyterLabTelemetryLibraryFactory,
+        IETCJupyterLabNotebookStateProvider
+    ],
+    activate: (app: JupyterFrontEnd, notebookTracker: INotebookTracker, etcJupyterLabTelemetryLibraryFactory: IETCJupyterLabTelemetryLibraryFactory, etcJupyterLabNotebookStateProvider: IETCJupyterLabNotebookStateProvider) => {
+        (async () => {
+            await app.started;
+            const settings = ServerConnection.makeSettings();
+            const baseUrl = settings.baseUrl;
+            let telemetryHandler = getTelemetryHandler(baseUrl, etcJupyterLabNotebookStateProvider);
+            try {
+                notebookTracker.widgetAdded.connect(
+                    (async (sender: INotebookTracker, notebookPanel: NotebookPanel) => {
+                        //  Handlers must be attached immediately in order to detect early events, hence we do not want to await the appearance of the Notebook.
+                        let etcJupyterLabTelemetryLibrary = etcJupyterLabTelemetryLibraryFactory.create({
+                            notebookPanel
+                        });
+                        etcJupyterLabNotebookStateProvider.addNotebookPanel({
+                            notebookPanel
+                        });
+                        etcJupyterLabTelemetryLibrary.notebookClipboardEvent.notebookClipboardCopied.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.notebookClipboardEvent.notebookClipboardCut.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.notebookClipboardEvent.notebookClipboardPasted.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.notebookVisibilityEvent.notebookVisible.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.notebookVisibilityEvent.notebookHidden.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.notebookOpenEvent.notebookOpened.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.notebookCloseEvent.notebookClosed.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.notebookSaveEvent.notebookSaved.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.notebookScrollEvent.notebookScrolled.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.activeCellChangeEvent.activeCellChanged.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.cellAddEvent.cellAdded.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.cellRemoveEvent.cellRemoved.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.cellExecutionEvent.cellExecuted.connect(telemetryHandler);
+                        etcJupyterLabTelemetryLibrary.cellErrorEvent.cellErrored.connect(telemetryHandler);
+                    }));
+            } catch (e) {
+                console.error(e);
+            }
+        })();
+    }
 };
-
 export default plugin;
-
